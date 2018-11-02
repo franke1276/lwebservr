2fd189bc413aa21a4c67f9a6fd999412f922bef4
Christian Franke <chriss@frankeonline.net>
2018-11-02 10:57:46 +0100

    use error chain for better error msgs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate clap;
 extern crate regex;
 
+#[macro_use]
+extern crate error_chain;
+
 use clap::{App, Arg};
 use regex::Regex;
 use std::fs;
@@ -8,13 +11,34 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+
+mod errors {
+    error_chain!{}
+}
+
+use errors::*;
+
+
 fn main() {
+     if let Err(ref e) = run() {
+        println!("error: {}", e);
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
     let re = Regex::new(r"([^ ]+) /([^ ]*) (.*)").unwrap();
 
     let matches = App::new("lwebservr")
         .version("1.0")
         .author("chriss@frankeonline.net")
-        .about("Serve local files as webserver")
+        .about("Serve local files via http")
         .arg(
             Arg::with_name("port")
                 .short("p")
@@ -37,27 +61,30 @@ fn main() {
         .value_of("port")
         .unwrap_or("8080")
         .parse::<u16>()
-        .expect("port must be a number between 1 and 65535");
+        .chain_err(|| "port must be a number between 1 and 65535")?;
 
     let verbose = matches.is_present("verbose");
     let silent = matches.is_present("silent");
 
     if !silent {
-    println!(
-        "Starting webserver on port {}, files will be served from {}",
-        port,
-        std::env::current_dir().unwrap().display()
-    );
+        println!(
+            "Starting webserver on port {}, files will be served from {}",
+            port,
+            std::env::current_dir().unwrap().display()
+        );
     }
 
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).chain_err(|| format!("could not bind to 127.0.0.1:{}", port))?;
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         handle_connection(stream, verbose, silent,&re);
     }
+    Ok(())
 }
+
+
 
 #[derive(Debug)]
 struct HttpResult {
@@ -138,7 +165,7 @@ fn handle_connection(mut stream: TcpStream, verbose: bool, silent: bool, re: &Re
     }
 }
 
-fn handle_get(path: &str) -> Result<HttpResult, HttpResult> {
+fn handle_get(path: &str) -> Result<HttpResult> {
     let filename = match path {
         "" => "index.html",
         p => p,
